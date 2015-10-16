abfdec8c757f85e7a7ff0cc34ef8ddf0a91468c4
Marc-Antoine Rondel <rondel-ma@hotmail.com>
2015-10-16 14:34:33 +0200

    modified:   plugin.xml 	renamed:    src/android/Toast.java -> src/android/ToastCordova.java

--- src/android/ToastCordova.java
+++ src/android/ToastCordova.java
@@ -4,18 +4,17 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
-import android.R;
-import android.content.Context;
-import android.content.DialogInterface;
-import android.app.AlertDialog;
 import android.widget.Toast;
+import android.content.Context;
 
-public class Toast extends CordovaPlugin {
+public class ToastCordova extends CordovaPlugin {
+
+  Context context=this.cordova.getActivity().getApplicationContext();
 
   public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
     final String msg = args.getString(0);
     if (action.equals("short")) {
-      final String msg = args.getString(0);
+
       showToast(msg,Toast.LENGTH_SHORT);
     }
 
@@ -27,7 +26,7 @@ public class Toast extends CordovaPlugin {
   }
 
   private void showToast(String content,Int size){
-  Toast.makeText(getApplicationContext(), content, size).show();
+  Toast.makeText(context, content, size).show();
   }
 
 }
